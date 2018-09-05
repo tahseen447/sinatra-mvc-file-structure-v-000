95b01a41e16697fa87585c094c99bc868950cc92
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-05 05:12:46 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,15 @@
 class Dog
   attr_accessor :name, :breed, :age
-  @@all=[]
-
-  def initialization(name, breed, age)
-    binding.pry
+  DOGS = []
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@all << self
+    DOGS << self
   end
 
-  def all
-    @@all
+  def self.all
+    DOGS
   end
+
 end
